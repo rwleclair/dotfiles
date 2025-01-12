ebedf2118a4b575103c58425b5202440a9c7871a
rwleclair <rwleclairr@gmail.com>
2025-01-12 16:43:30 -0600

    added surfingkeys config

--- nvim/.config/nvim/lua/ryan/core/keymaps.lua
+++ nvim/.config/nvim/lua/ryan/core/keymaps.lua
@@ -4,7 +4,6 @@ local keymap = vim.keymap -- for conciseness
 
 ---------------------
 -- General Keymaps -------------------
-
 -- use jk to exit insert mode
 keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
 -- clear search highlights
@@ -19,13 +18,13 @@ keymap.set("n", "<leader>+", "<C-a>", { desc = "Increment number" }) -- incremen
 keymap.set("n", "<leader>-", "<C-x>", { desc = "Decrement number" }) -- decrement
 
 -- window management
-keymap.set("n", "<leader>s", "", { desc  = "Tab splitting commands"})
+keymap.set("n", "<leader>s", "", { desc = "Tab splitting commands" })
 keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) -- split window vertically
 keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
 keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
 keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
 
-keymap.set("n", "<leader>t", "", { desc  = "Tab commands"})
+keymap.set("n", "<leader>t", "", { desc = "Tab commands" })
 keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" }) -- open new tab
 keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" }) -- close current tab
 keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
